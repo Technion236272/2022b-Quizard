e8363c34dc900bceee26fa9628ac4aad7b3f8c26
raz_a <razas@campus.technion.ac.il>
2022-05-09 15:52:33 +0300

    ui-first-screen

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(222, 203, 57, 1);
+const defaultColor = Colors.black;
+const textFieldBackgroundColor = Colors.white;
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: Theme.of(context).copyWith(
+          colorScheme:
+              Theme.of(context).colorScheme.copyWith(primary: Colors.white),
+          scaffoldBackgroundColor: backgroundColor),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  void initState() {
+    super.initState();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    // Hide StatusBar, Show navigation buttons
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Padding(
+      padding: const EdgeInsets.all(12),
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
+              Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const <Widget>[
+            InkWell(
+              child: Icon(
+                Icons.language,
+                color: defaultColor,
+                size: 32.0,
+              ),
+              onTap: null, // TODO: Go to Change Language screen
+            )
+          ],
+        ),
+        const Image(image: AssetImage('images/titles/quizard.png')),
+        const Text(
+          'Please login to your account',
+          style: TextStyle(fontSize: 18),
+        ),
+        Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+              child: TextFormField(
+                  cursorColor: defaultColor,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: textFieldBackgroundColor,
+                    border: OutlineInputBorder(),
+                    hintText: 'Username / Email',
+                  ))),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+              child: TextFormField(
+                  cursorColor: defaultColor,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: textFieldBackgroundColor,
+                    border: OutlineInputBorder(),
+                    hintText: 'Password',
+                  ))),
+        ]),
+        ElevatedButton(
+          child: const Text('Log in', style: TextStyle(color: defaultColor)),
+          onPressed: () {}, //TODO: Login with Email
+        ),
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Row(children: const <Widget>[
+              // OR Divider
+              Expanded(child: Divider(color: defaultColor)),
+              Text("  OR  "),
+              Expanded(child: Divider(color: defaultColor)),
+            ])),
+        Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton.icon(
+              onPressed: () {}, //TODO: Continue with Google
+              label: const Text('Continue with Google',
+                  style: TextStyle(color: defaultColor)),
+              icon: const FaIcon(FontAwesomeIcons.google, color: defaultColor),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton.icon(
+              onPressed: () {}, //TODO: Continue with Facebook
+              label: const Text('Continue with Facebook',
+                  style: TextStyle(color: defaultColor)),
+              icon:
+                  const FaIcon(FontAwesomeIcons.facebook, color: defaultColor),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Text("Don't have an account? "),
+              InkWell(
+                child: Text('Sign Up'),
+                onTap: null, //TODO: Go to Sign Up screen
+              )
+            ])
+      ]),
+    ));
   }
 }
