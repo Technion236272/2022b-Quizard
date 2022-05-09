968ce3738d9399222ac413a14b5b8e56c475f8a0
raz_a <razas@campus.technion.ac.il>
2022-05-09 19:57:50 +0300

    #4: disable log in button while logging in is supported now

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,6 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     final loginModel = Provider.of<LoginModel>(context, listen: false);
-
     void _loginNow() {
       loginModel.toggleLogging();
       AuthModel.instance()
@@ -66,6 +65,7 @@ class _WelcomePageState extends State<WelcomePage> {
           .then((result) async {
         if (result == true) {
           loginModel.logIn();
+          loginModel.toggleLogging();
           Navigator.of(context).push(
               MaterialPageRoute<void>(builder: (context) => const HomePage()));
         } else {
@@ -80,98 +80,104 @@ class _WelcomePageState extends State<WelcomePage> {
       });
     }
 
-    return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(12),
-      child:
-          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
-              Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const <Widget>[
-            InkWell(
-              child: Icon(
-                Icons.language,
-                color: defaultColor,
-                size: 32.0,
+    return Consumer<LoginModel>(builder: (context, loginModel, child) {
+      return Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const <Widget>[
+                  InkWell(
+                    child: Icon(
+                      Icons.language,
+                      color: defaultColor,
+                      size: 32.0,
+                    ),
+                    onTap: null, // TODO: Go to Change Language screen
+                  )
+                ],
               ),
-              onTap: null, // TODO: Go to Change Language screen
-            )
-          ],
-        ),
-        const Image(image: AssetImage('images/titles/quizard.png')),
-        const Text(
-          'Please login to your account',
-          style: TextStyle(fontSize: 18),
-        ),
-        Column(children: <Widget>[
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-              child: TextFormField(
-                  controller: loginModel.emailController,
-                  cursorColor: defaultColor,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: textFieldBackgroundColor,
-                    border: OutlineInputBorder(),
-                    hintText: 'Username / Email',
-                  ))),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-              child: TextFormField(
-                  controller: loginModel.passwordController,
-                  cursorColor: defaultColor,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: textFieldBackgroundColor,
-                    border: OutlineInputBorder(),
-                    hintText: 'Password',
-                  ))),
-        ]),
-        ElevatedButton(
-          child: const Text('Log in', style: TextStyle(color: defaultColor)),
-          onPressed: loginModel.isLoggingIn
-              ? null
-              : _loginNow, //TODO: Login with Email
-        ),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Row(children: const <Widget>[
-              // OR Divider
-              Expanded(child: Divider(color: defaultColor)),
-              Text("  OR  "),
-              Expanded(child: Divider(color: defaultColor)),
-            ])),
-        Column(
-          children: <Widget>[
-            ElevatedButton.icon(
-              onPressed: () {}, //TODO: Continue with Google
-              label: const Text('Continue with Google',
-                  style: TextStyle(color: defaultColor)),
-              icon: const FaIcon(FontAwesomeIcons.google, color: defaultColor),
-            ),
-            ElevatedButton.icon(
-              onPressed: () {}, //TODO: Continue with Facebook
-              label: const Text('Continue with Facebook',
-                  style: TextStyle(color: defaultColor)),
-              icon:
-                  const FaIcon(FontAwesomeIcons.facebook, color: defaultColor),
-            ),
-          ],
-        ),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text("Don't have an account? "),
-              InkWell(
-                child: Text('Sign Up'),
-                onTap: null, //TODO: Go to Sign Up screen
-              )
-            ])
-      ]),
-    ));
+              const Image(image: AssetImage('images/titles/quizard.png')),
+              const Text(
+                'Please login to your account',
+                style: TextStyle(fontSize: 18),
+              ),
+              Column(children: <Widget>[
+                Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                    child: TextFormField(
+                        controller: loginModel.emailController,
+                        cursorColor: defaultColor,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: textFieldBackgroundColor,
+                          border: OutlineInputBorder(),
+                          hintText: 'Username / Email',
+                        ))),
+                Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                    child: TextFormField(
+                        controller: loginModel.passwordController,
+                        cursorColor: defaultColor,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: textFieldBackgroundColor,
+                          border: OutlineInputBorder(),
+                          hintText: 'Password',
+                        ))),
+              ]),
+              ElevatedButton(
+                child:
+                    const Text('Log in', style: TextStyle(color: defaultColor)),
+                onPressed: loginModel.isLoggingIn
+                    ? null
+                    : _loginNow, //TODO: Login with Email
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Row(children: const <Widget>[
+                    // OR Divider
+                    Expanded(child: Divider(color: defaultColor)),
+                    Text("  OR  "),
+                    Expanded(child: Divider(color: defaultColor)),
+                  ])),
+              Column(
+                children: <Widget>[
+                  ElevatedButton.icon(
+                    onPressed: () {}, //TODO: Continue with Google
+                    label: const Text('Continue with Google',
+                        style: TextStyle(color: defaultColor)),
+                    icon: const FaIcon(FontAwesomeIcons.google,
+                        color: defaultColor),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {}, //TODO: Continue with Facebook
+                    label: const Text('Continue with Facebook',
+                        style: TextStyle(color: defaultColor)),
+                    icon: const FaIcon(FontAwesomeIcons.facebook,
+                        color: defaultColor),
+                  ),
+                ],
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text("Don't have an account? "),
+                    InkWell(
+                      child: Text('Sign Up'),
+                      onTap: null, //TODO: Go to Sign Up screen
+                    )
+                  ])
+            ]),
+      ));
+    });
   }
 }
