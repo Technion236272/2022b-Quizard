df85ce17c30729482de997617bee9430005a9d6a
maysam00haj <maysam.hajyahia@outlook.com>
2022-05-31 16:11:53 +0300

    #37: first implementation of the score-board.

--- lib/game.dart
+++ lib/game.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quizard/home.dart';
 import 'package:quizard/providers.dart';
+import 'package:confetti/confetti.dart';
 import 'consts.dart';
+
+List<String> players = [];
 
 class Question extends StatelessWidget {
   final String _questionText;
@@ -96,43 +99,95 @@ class _AnswerState extends State<Answer> {
 }
 
 // Should be the score-board class.
-class Result extends StatelessWidget {
-  const Result({Key? key}) : super(key: key);
+class ScoreBoard extends StatefulWidget {
+  const ScoreBoard(
+      {Key? key, required this.pinCode})
+      : super(key: key);
+  final String pinCode;
 
   @override
+  _ScoreBoardState createState() => _ScoreBoardState();
+
+}
+
+class _ScoreBoardState extends State<ScoreBoard> {
+  late ConfettiController _controllerTopCenter;
+  @override
   Widget build(BuildContext context) {
+    _controllerTopCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+    _controllerTopCenter.play();
+    Future<List<String>> _buildPlayers() async {
+      await FirebaseFirestore.instance
+          .collection('versions/v1/custom_games')
+          .doc(widget.pinCode)
+          .get()
+          .then((game) {
+        players = List<String>.from(game["participants"]);
+      });
+
+      for(var name in players)
+      {
+
+
+      }
+
+      return players;
+    }
+
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 50,
-          backgroundColor: backgroundColor,
-          toolbarOpacity: 0,
-          elevation: 0,
-        ),
-        backgroundColor: backgroundColor,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Score ' '100',
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+        body: Container(
+            child: Column( children: [
+              const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 100),
+                  child: Image(
+                      image:
+                      AssetImage('images/titles/winner.png'))),
+              FutureBuilder(
+                future: _buildPlayers(),
+                builder: (BuildContext context, AsyncSnapshot snapshot){
+                  print(snapshot.data);
+                  if(snapshot.data == null){
+                    return Container(
+                        child: Center(
+                            child: Text("Loading...")
+                        )
+                    );
+                  } else {
+                    return Center(child:
+                    Column(children:[
+                      ConfettiWidget(
+                        confettiController: _controllerTopCenter,
+                        blastDirection: 7,
+                        shouldLoop: false,
+                      ),
+                      Container(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          height: 400,
+                          width: 350,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: secondaryColor,
+                          ),
+                          child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      "https://www.google.com/search?q=image&sxsrf=ALiCzsYILxEWE940ni0LAbcujZ3rgC1VDg:1653996309044&tbm=isch&source=iu&ictx=1&vet=1&fir=sp12V8x9gw6KuM%252C4O2GvGuD-Cf09M%252C_%253BgxFxsvFBmxeZ9M%252C0JWe7yDOKrVFAM%252C_%253BDH7p1w2o_fIU8M%252CBa_eiczVaD9-zM%252C_%253BQOZymhPf48LDYM%252CLOSptVP0p_ZwUM%252C_%253Bn5hAWsQ-sgKo_M%252C-UStXW0dQEx4SM%252C_%253B2DNOEjVi-CBaYM%252CAOz9-XMe1ixZJM%252C_%253B0DzWhtJoQ1KWgM%252CcIQ7wXCEtJiOWM%252C_%253Bz4_uU0QB2pe-SM%252C7SySw5zvOgPYAM%252C_%253B2nDXavJs9DoKTM%252CB51x0PBR9KNzvM%252C_%253BsI3XXpFjQg61vM%252C0_HmqFdutkPVdM%252C_%253BMOAYgJU89sFKnM%252CygIoihldBPn-LM%252C_%253BuPicAfWpubBp4M%252C_Hlm4-qqBRN0IM%252C_&usg=AI4_-kRJA4O93E7EcUkikx5uVNLogU3cNA&sa=X&ved=2ahUKEwiF99az0In4AhWE8LsIHXFZBGgQ9QF6BAgCEAE#imgrc=sp12V8x9gw6KuM"
+                                  ),
+                                ),
+                                title: Text(snapshot.data[index]),
+                              );
+                            },
+                          ))]));
+                  }
+                },
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 50),
-                child: ElevatedButton(
-                  child: const Text(
-                    'Exit Quiz',
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute<void>(
-                        builder: (context) => const HomePage()));
-                  },
-                ),
-              )
-            ],
-          ),
-        ));
+            ])));
   }
 }
 
@@ -223,7 +278,7 @@ class _SecondGameScreenState extends State<SecondGameScreen> {
                           } else {
                             gameModel.currentQuestionIndex = 0;
                             Navigator.of(context).push(MaterialPageRoute<void>(
-                                builder: (context) => const Result()));
+                                builder: (context) => ScoreBoard(pinCode: gameModel.pinCode)));
                           }
                         });
                         return const Padding(
