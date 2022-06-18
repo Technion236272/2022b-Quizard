35332bccbb2ebc9409e4bd66ba58eadad6a0e8fe
Raz <razash10@gmail.com>
2022-06-18 10:31:49 +0300

    #72: added game builder as cloud function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,12 +2,12 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 admin.initializeApp();
+const db = admin.firestore();
+const maxPlayers = 2; // TODO: Change to 5
 
 export const findQuickGameForNewPlayer = functions.firestore
     .document("versions/{v2}/official_games/{waiting_room}/players/{playerId}")
     .onCreate(async (player, context) => {
-      const db = admin.firestore();
-      const maxPlayers = 2; // TODO: Change to 5
       const playerUsername = await player.get("username");
       const playerId = context.params.playerId;
       const gamesRef = db.collection("versions/v2/official_games");
@@ -128,4 +128,100 @@ export const findQuickGameForNewPlayer = functions.firestore
 
         return;
       });
+    });
+
+/**
+ * @param {number} min The first number.
+ * @param {number} max The second number.
+ * @return {number} Random number between min (inclusive) and max (inclusive).
+ */
+function getRandomBetween(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const roundsPerGame = 5;
+
+export const initiateGameForFullParty = functions.firestore
+    .document("versions/{v2}/official_games/{changedGameId}")
+    .onWrite(async (changedGame) => {
+      if (!changedGame.after.exists) {
+        // Ignore delete operations
+        console.log("ignoring delete operation");
+        return;
+      }
+
+      const isLocked = await changedGame.after.get("is_locked");
+      if (isLocked) {
+        // Ignore built games
+        console.log("ignoring built game");
+        return;
+      }
+
+      // check if party is full
+      for (let i = 0; i < maxPlayers; i++) {
+        const username = await changedGame.after.get(`player${i}.username`);
+        if (username == "") {
+          console.log("party is not full yet");
+          return;
+        }
+      }
+
+      // from here party is full and we can start game
+      console.log("party is full. initiating game...");
+
+      // gather all trivia data first
+      const allQuestions: string[] = [];
+      const allAnswers: string[] = [];
+      const allCategories: string[] = [];
+      const questionsRef = db.collection("versions/v1/official_questions");
+      const triviaSnap = await questionsRef.get();
+      const numOfCategories = triviaSnap.docs.length;
+      console.log("number of categories on firestore: ", numOfCategories);
+      for (let i = 0; i < numOfCategories; i++) {
+        const categoryDoc = triviaSnap.docs[i];
+        const categoryStr = categoryDoc.id;
+        const questions: string[] = await categoryDoc.get("questions");
+        const answers: string[] = await categoryDoc.get("answers");
+        for (let j = 0; j < questions.length; j++) {
+          allQuestions.push(questions[j]);
+          allAnswers.push(answers[j]);
+          allCategories.push(categoryStr);
+        }
+      }
+
+      console.log("built trivia. size: ", allQuestions.length);
+      console.log("choosing random indexes...");
+
+      // generate unique random indexes
+      const randomIndexes: number[] = [];
+      while (randomIndexes.length < roundsPerGame) {
+        const random = getRandomBetween(0, allQuestions.length-1);
+        if (!randomIndexes.includes(random)) {
+          randomIndexes.push(random);
+        }
+      }
+
+      console.log("generated indexes. building questions...");
+
+      // build the trivia
+      const builtQuestions = [];
+      const builtAnswers = [];
+      const builtCategories = [];
+      for (let i =0; i < roundsPerGame; i++) {
+        builtQuestions.push(allQuestions[randomIndexes[i]]);
+        builtAnswers.push(allAnswers[randomIndexes[i]]);
+        builtCategories.push(allCategories[randomIndexes[i]]);
+      }
+
+      console.log("new game is ready. updating firestore...");
+
+      await changedGame.after.ref.update(
+          {"questions": builtQuestions,
+            "answers": builtAnswers,
+            "categories": builtCategories,
+            "is_locked": true,
+            "is_official": true});
+
+      console.log("done.");
+      return;
     });
